6e411caa3fc443a2363de1460868e200df84351a
Jintao <jintao081@qq.com>
2026-07-22 20:36:14 +0800

    Update ipcheck to 0.4.0

--- Formula/ipcheck.rb
+++ Formula/ipcheck.rb
@@ -1,8 +1,8 @@
 class Ipcheck < Formula
   desc "Diagnose Codex and Claude Code network paths"
   homepage "https://github.com/jacklv-coder/ipcheck"
-  url "https://github.com/jacklv-coder/ipcheck/releases/download/v0.3.0/ipcheck-0.3.0.tar.gz"
-  sha256 "a077b2575f6c7c45f00f0ac61e3b6c860aa57ce74f493dff6d94b4de1ce9a826"
+  url "https://github.com/jacklv-coder/ipcheck/releases/download/v0.4.0/ipcheck-0.4.0.tar.gz"
+  sha256 "603e8bb2daf13a3aaadd7df35e3efd2a6e1f2b6589e682bd8689fd8a77bd5972"
   license "Apache-2.0"
 
   def install
